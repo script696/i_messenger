7cf0c2ddc483d1fe05bebf37b9f3454a793790a4
Nikita <script696@yandex.ru>
2022-08-19 13:52:07 +0300

    Added navigation bar
    Added navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Bell from './pages/Bell/Bell';
-import Messenger from './pages/Messenger/Messenger';
-import Operator from './pages/Operator/Operator';
-import Question from './pages/Question/Question';
-import Phone from './shared/Phone/Phone';
+import Bell from "./pages/Bell/Bell";
+import Messenger from "./pages/Messenger/Messenger";
+import Operator from "./pages/Operator/Operator";
+import Question from "./pages/Question/Question";
+import Phone from "./shared/Phone/Phone";
 import "./styles/index.scss";
-
 
 function App() {
   return (
     <div className="container">
       <Phone>
-      <Switch>
-        <Route path='/' exact component={Messenger}/>
-        <Route path='/question' exact component={Question}/>
-        <Route path='/bell' exact component={Bell}/>
-        <Route path='/operator' exact component={Operator}/>
-      </Switch>
+        <Switch>
+          <Route path="/messenger" exact component={Messenger} />
+          <Route path="/question" exact component={Question} />
+          <Route path="/bell" exact component={Bell} />
+          <Route path="/operator" exact component={Operator} />
+        </Switch>
       </Phone>
     </div>
   );
