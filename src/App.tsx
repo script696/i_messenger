70273d1a2ac98ed833fd708dac7974ec0d9f9153
Nikita <script696@yandex.ru>
2022-08-19 09:10:49 +0300

    Added router to project
    Added router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Bell from './pages/Bell/Bell';
 import Messenger from './pages/Messenger/Messenger';
+import Operator from './pages/Operator/Operator';
+import Question from './pages/Question/Question';
 
 function App() {
   return (
     <div className="App">
-      <Messenger/>
+      <Switch>
+        <Route path='/' exact component={Messenger}/>
+        <Route path='/question' exact component={Question}/>
+        <Route path='/bell' exact component={Bell}/>
+        <Route path='/operator' exact component={Operator}/>
+      </Switch>
     </div>
   );
 }
